7feff551648b3ed22564351e39931fffa976c7e6
Zachary Boyd <zachary@zacharyboyd.nyc>
2022-01-11 14:09:31 -0500

    inital

--- lib/Atticfile.d.ts
+++
@@ -1,9 +0,0 @@
-import { IPlugin } from "@znetstar/attic-common/lib/Server";
-import { AtticServerNotificationsApplicationContext } from "./common";
-export declare class AtticServerNotifications implements IPlugin {
-    applicationContext: AtticServerNotificationsApplicationContext;
-    constructor(applicationContext: AtticServerNotificationsApplicationContext);
-    init(): Promise<void>;
-    get name(): string;
-}
-export default AtticServerNotifications;
